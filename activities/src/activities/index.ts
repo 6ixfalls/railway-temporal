8518dbad66d14360e8a26e11a0db967c5c59624e
Six <23470032+6ixfalls@users.noreply.github.com>
2024-07-12 11:24:18 +0800

    feat: get correct broadcast address

--- activities/src/activities/index.ts
+++
@@ -1,3 +0,0 @@
-export { cancellableFetch } from './cancellable-fetch';
-export { makeHTTPRequest } from './make-http-request';
-export { doSomethingAsync as completeSomethingAsync } from './async-completion';
---
+++ railway.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+# https://superuser.com/a/1403037
+export TEMPORAL_BROADCAST_ADDRESS="$(ip -6 addr|awk '{print $2}'|grep -P '^(?!fe80)[[:alnum:]]{4}:.*/64'|cut -d '/' -f1)"
+echo "$TEMPORAL_BROADCAST_ADDRESS"
+
+/etc/temporal/entrypoint.sh "$@"
