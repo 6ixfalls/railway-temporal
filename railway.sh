710ad1eb963e8f5c8cbfeb6711e06899b3cfc070
Six <23470032+6ixfalls@users.noreply.github.com>
2024-07-13 04:21:08 -0700

    Update railway.sh

--- railway.sh
+++ railway.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 set -eu -o pipefail
-
 # https://superuser.com/a/1403037
 export TEMPORAL_BROADCAST_ADDRESS="$(ip -6 addr|awk '{print $2}'|grep -P '^(?!fe80)[[:alnum:]]{4}:.*/64'|cut -d '/' -f1)"
 echo "$TEMPORAL_BROADCAST_ADDRESS"
