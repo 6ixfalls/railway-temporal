736a073bc24e1ed8a1a1bcdad634f4162a59c1e9
Six <23470032+6ixfalls@users.noreply.github.com>
2024-07-23 02:25:04 +0800

    fix: cleanup script [skip ci]

--- railway.sh
+++ railway.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 set -eu -o pipefail
-ip -6 addr
-ip addr show dev railnet0 | sed -e's/^.*inet6 \([^ ]*\)\/.*$/\1/;t;d'
 # https://superuser.com/a/1403037
 export TEMPORAL_BROADCAST_ADDRESS="$(ip -6 addr|awk '{print $2}'|ack -o '^(?!fe80)[[:alnum:]]{4}:.*/64'|cut -d '/' -f1)"
-echo "$TEMPORAL_BROADCAST_ADDRESS"
-
+echo "Broadcasting address $TEMPORAL_BROADCAST_ADDRESS"
 echo "Starting Temporal server"
-
 /etc/temporal/entrypoint.sh "$@"
