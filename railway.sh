9acbb23dca3ec24352406ff29e27a82ccd15d404
Six <23470032+6ixfalls@users.noreply.github.com>
2024-09-22 01:56:30 -0700

    feat: add args debug

--- railway.sh
+++ railway.sh
@@ -5,4 +5,5 @@ set -eu -o pipefail
 export TEMPORAL_BROADCAST_ADDRESS="$(ip -6 addr|awk '{print $2}'|ack -o '^(?!fe80)[[:alnum:]]{4}:.*/64'|cut -d '/' -f1)"
 echo "Broadcasting address $TEMPORAL_BROADCAST_ADDRESS"
 echo "Starting Temporal server"
+echo "Args: $@"
 /etc/temporal/entrypoint.sh "$@"
